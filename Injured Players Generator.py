69e5122c63762b36cdcfd252fef6fe084f58882f
jlev8327 <59658974+jlev8327@users.noreply.github.com>
2021-12-08 22:49:25 -0500

    Update Injured Players Generator.py

--- Injured Players Generator.py
+++ Injured Players Generator.py
@@ -3,8 +3,8 @@ import numpy as np
 import nba_api.stats.endpoints
 
 games = pd.read_csv('games.csv', dtype=str)
-games = games[games["GAME_DATE"]>"2018-10-15"]
-games = games[games["GAME_DATE"]<"2019-10-21"]
+games = games[games["GAME_DATE"]>"2020-10-15"]
+games = games[games["GAME_DATE"]<"2021-12-8"]
 
 def player_out(gameid):
     return [" ".join(x[1:3]) for x in nba_api.stats.endpoints.BoxScoreSummaryV2(gameid).inactive_players.data['data']]
@@ -13,10 +13,10 @@ outs = []
 
 #1597 total games
 
-for x in games["GAME_ID"][1500:]:
+for x in games["GAME_ID"][:100]:
     players = player_out(x)
     outs += [[x, y] for y in players]
 
 outs = pd.DataFrame(outs, columns=['GAME_ID', 'Player'])
 
-outs.to_csv("player_out42.csv")
+outs.to_csv("player_out1.csv")
